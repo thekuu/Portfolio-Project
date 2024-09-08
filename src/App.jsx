3cd39c02b683afa5da7a64b0bcac1ff27a167298
thekuu <127185902+thekuu@users.noreply.github.com>
2024-09-08 17:43:01 +0300

    align center

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import Services from './components/Services/Services'
 import Mywork from './components/My-Works/Mywork'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import './App.css'
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Services/>
-      <Mywork/>
-      <Contact/>
-      <Footer/>
+    <div className = "app">
+      <div>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Services/>
+        <Mywork/>
+        <Contact/>
+        <Footer/>
+      </div>
     </div>
   )
 }
